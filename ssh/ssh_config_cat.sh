216505496418cdf17658a3a99c171f3bbde5c44c
MT <mtan.web+github@gmail.com>
2019-07-11 23:03:18 +0900

    Fix errors

--- ssh/ssh_config_cat.sh
+++ ssh/ssh_config_cat.sh
@@ -7,6 +7,7 @@ BASE="${THIS%.*}"
 
 # Vars
 ssh_config=""
+ssh_cnfdir=""
 
 # Flags
 rm_comment=0
@@ -55,35 +56,53 @@ _MSG_
   exit 1
 }
 
+# SSH Config Dir
+ssh_cnfdir="${ssh_config%/*}"
+
 # Print
 cat "${ssh_config}" |
 while read row_data
 do
 
-  [ $rm_comment -ne 0 -a -n "${row_data}" ] && {
-    row_data=$(echo "${row_data%#*}")
-    [ -n "${row_data}" ] &&
+  if [ $rm_comment -ne 0 ]
+  then
+    if [ -n "${row_data}" ]
+    then
+      row_data=$(echo ${row_data%%#*})
+    fi
+    if [ -z "${row_data}" ]
+    then
       continue
-  } || :
+    fi
+  fi || :
 
   printf "%s" "${row_data}" |
   egrep -i '^[ \t]*include[ \t]+[^ \t].*$' 1>/dev/null || {
-    echo "${row_data}"
+    printf "%s" "${row_data}"; echo
     continue
   }
 
-  row_data="${row_data%#*}"
-
+  inc_file="${row_data%%#*}"
   inc_file=$(
-    echo "${row_data#*nclude}" |tr '\t' ' ' |
+    echo "${inc_file#*nclude}" |tr '\t' ' ' |
     sed -e 's;  *; ;g' -e 's;^ *;;g' -e 's; *$;;')
 
   if [ $ignore_inc -eq 0 ]
   then
+
     echo "# <<< Include ${inc_file}"
-    ( cd "${ssh_config%/*}" &&
-      cat ${inc_file} /dev/null; )
+
+    ( if [ -n "${ssh_cnfdir}" ]
+      then cd "${ssh_cnfdir}"
+      else :
+      fi
+      if [ -z "${inc_file}" ]
+      then cat ${inc_file}
+      else echo "# ERROR: '${inc_file}': no such file or dir."
+      fi; )
+
     echo "# >>> Include ${inc_file}"
+
   else
     echo "# <<< Include ${inc_file} >>>"
   fi
